b0be094c2e9ff8204e18599df1f2eefabd333ad4
z0di4ckX <ultimatecracker88@gmail.com>
2019-12-06 20:22:17 -0400

    delete categoria file

--- categories/Ani.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    include('../connect.php');
-
-    $query = "SELECT * FROM pelicula WHERE categoria = 6";
-    $ani = $conn->query($query);
-
-?>
--- categories/Terror.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    include('../connect.php');
-
-    $query = "SELECT * FROM pelicula WHERE categoria = 5";
-    $terror = $conn->query($query);
-
-?>
