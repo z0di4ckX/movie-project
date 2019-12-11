63c1b6f45d1cdff6fad11ac3dbde44818ca09f96
Wesly <ultimatecracker88@gmail.com>
2019-12-10 22:35:46 -0400

    modefied db

--- include/connect.php
+++ include/connect.php
@@ -3,7 +3,7 @@
     $host = "localhost";
     $username = "root";
     $password = "";
-    $db = "interflixdb";
+    $db = "movie_project";
 
     $conn = mysqli_connect($host, $username, $password, $db);
 
